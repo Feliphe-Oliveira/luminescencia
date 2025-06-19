d339913f3094cb8e7b2735db3715b873a579cf9c
Feliphe-Oliveira <oliveirafeliphe272@gmail.com>
2025-06-19 20:43:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('btnLum').addEventListener('click',()=>{
-    const cod = 'descobrir'
+    const cod = 'eternizar'
     if(document.getElementById('senha').value.toUpperCase() == cod.toUpperCase()){
         document.getElementById('cx_verso').classList.add('destaque')
         document.getElementById('login').classList.add('semdestaque')
